fec27bd32b086dc20ad31437d24e9b5f8b3fd19a
JuliaTurok <107355921+JuliaTurok@users.noreply.github.com>
2022-10-05 12:40:35 +0300

    Added api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function LoginForm() {
   let [keyword, setKeyword] = useState("");
+  function handleResponse(response){
+ console.log(response.data[0])
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse)
   }
 
   function updateKeyword(event) {
